0aaa7a79833d49464afe442de81751f2169eb4ef
sonuravi2705-creator <sonuravi2705@gmail.com>
2026-05-13 12:52:50 +0000

    fix supergroup id

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import requests
 
 BOT_TOKEN = "8651727429:AAHAA9nFtPpUO2npxgdR6MyZkZBMqHLyTRg"
-CHAT_ID = "-4037707574219"
+CHAT_ID = "-1002706339720"
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
